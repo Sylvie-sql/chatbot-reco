e98d8970035d0288f06373d557c961464c6db532
yw <yue.wu@sage.com>
2025-04-10 10:25:12 +0200

    deploy to GitHub Pages

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ChatbotReco from "./src/ChatbotReco";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ChatbotReco />
-  </React.StrictMode>
-);
